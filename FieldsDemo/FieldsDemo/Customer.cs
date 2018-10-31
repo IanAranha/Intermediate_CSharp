3362889ee5000bdd435400678e775dee386e5a2f
Ian Aranha <ianorama@gmail.com>
2018-10-31 10:35:57 -0700

    Added the ListDemo from Udemy course

---
+++ FieldsDemo/FieldsDemo/Customer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace FieldsDemo
+{
+    public class Customer
+    {
+        public int ID;
+        public string Name;
+
+        public List<Order> Orders = new List<Order>();
+
+        public Customer(int id)
+        {
+            this.ID = id;
+        }
+
+        public Customer(int id, string name)
+            : this(id)
+        {
+            this.Name = name;
+        }
+
+        public void Promote()
+        {
+            // some code here....
+            //The code in the class intializes the List Order.
+            //Suppose in our main class we add some orders to List, 
+            //if we accidently re-initialize HERE, the orders already added
+            //will be wiped out.
+
+            Orders = new List<Order>();
+        }
+    }
+}
---
+++ FieldsDemo/FieldsDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace FieldsDemo
+{
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //Create a new customer
+            var customer = new Customer(1);
+
+            //Add two orders to the customers list
+
+            customer.Orders.Add(new Order());
+            customer.Orders.Add(new Order());
+
+            //If we display the Orders, we should see two items.
+            Console.WriteLine(customer.Orders.Count);
+
+            //Now, if we call the promote() method which is in the Customer object
+            //That will reinitialize the list and set it to zero
+
+            customer.Promote();
+            Console.WriteLine(customer.Orders.Count);
+
+            //The way around this is to set the List to READ ONLY.
+            //in the customer class, line 10 should become
+            //public  readonly List<Order> Orders = new List<Order>();
+
+            // if you try to initialize the list elsewhere in code, the IDE will throw an error BEFORE
+            //it compiles, runs the code.
+
+        }
+    }
+}
